085aee776ce6890eac0314dcd72e3b28d5eafda5
Muhammet ATAR <atarfikret146@gmail.com>
2022-11-08 22:33:56 +0300

    Add files via upload

---
+++ hww.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using System.Globalization;
+public class hww : MonoBehaviour
+{
+    float x1, y1, z1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("fikret");
+        FileInfo theSourceFile = new FileInfo(@"C:\Users\atarf\Desktop\arodev2\file1.txt");
+        StreamReader reader = theSourceFile.OpenText();
+
+        string text;
+        //print("fikret");
+        //Debug.Log("fikret");
+     
+        int sayac = 0;
+      //  int ilk = 1;
+        
+        int flag = 0;
+       
+        do
+        {
+            text = reader.ReadLine();
+           // Console.WriteLine(text);
+            //string phrase = "The quick brown fox jumps over the lazy dog.";
+            
+            if (text==null)
+            {
+                break;
+            }
+            string[] words = text.Split(' ');
+            
+            foreach (var word in words)
+            {
+                if (sayac == 1)
+                {
+                   x1=float.Parse(word, CultureInfo.InvariantCulture.NumberFormat);
+                    //x1 = (float)Convert.ToFL(word);
+
+                    sayac++;
+                }
+                else if (sayac == 2)
+                {
+                    y1=float.Parse(word, CultureInfo.InvariantCulture.NumberFormat);
+                    //y1 = (float)Convert.ToDouble(word);
+                    sayac++;
+                }
+                else if (sayac == 3)
+                {
+                    z1=float.Parse(word, CultureInfo.InvariantCulture.NumberFormat);
+                    //z1 = (float)Convert.ToDouble(word);
+                    flag = 1;
+                    sayac =1;
+
+                }
+                else if (sayac == 0)
+                {
+                    sayac++;
+                    break;
+                }
+
+                if (flag == 1)
+                {
+                    Debug.Log(x1);
+                    Debug.Log(y1);
+                    Debug.Log(z1);
+                    GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                    sphere.transform.position = new Vector3(x1, y1, z1);
+                    sphere.GetComponent<Renderer>().material.color = new Color32(0, 47, 58, 90);
+                    Debug.Log("ilk küp ok");
+
+                    flag = 0;
+                }
+                
+
+
+               // Debug.Log($"<{word}>");
+
+            }
+            if (text != null)
+            {
+              //  Debug.Log(text);
+            }
+            
+           // print(text);
+           // print("fikret");
+
+        } while (text != null);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
